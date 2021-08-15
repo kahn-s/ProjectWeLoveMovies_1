235101c31c8f0845caffa62d746779a9c451e66a
Shari Kahn <kahn.shari2@gmail.com>
2021-08-14 22:55:57 -0400

    added movie data to theeaters route

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -5,11 +5,15 @@ const reduceMovies = reduceProperties("theater_id", {
   movie_id: ["movies", null, "movie_id"],
   title: ["movies", null, "title"],
   rating: ["movies", null, "rating"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+  description: ["movies", null, "description"],
+  image_url: ["movies", null, "image_url"],
+  created_at: ["movies", null, "created_at"],
 });
 
 function list() {
   return knex("theaters as t")
-    .select("t.theater_id", "t.name")
+    .select("t.*", "m.*")
     .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
     .join("movies as m", "mt.movie_id", "m.movie_id")
     .then(reduceMovies);
